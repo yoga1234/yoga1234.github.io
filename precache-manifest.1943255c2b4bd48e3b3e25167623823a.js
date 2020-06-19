9e42346e7239a7aedb1e446051561f127abaa438
Septiana Yoga <septianayoga30@gmail.com>
2020-06-19 21:01:34 +0700

    deploy

---
+++ precache-manifest.1943255c2b4bd48e3b3e25167623823a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "33b790b9d6526c008312",
+    "url": "/static/css/main.53773c0c.chunk.css"
+  },
+  {
+    "revision": "33b790b9d6526c008312",
+    "url": "/static/js/main.33b790b9.chunk.js"
+  },
+  {
+    "revision": "5c38f0c3226b66d6d039",
+    "url": "/static/js/1.5c38f0c3.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "75c1ad7ebabb8b5d0424444a3e611379",
+    "url": "/index.html"
+  }
+];
